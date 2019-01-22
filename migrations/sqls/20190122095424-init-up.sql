270d817c5505f49926a10b58b18617b08a2168e3
Andreas Helgegren <andreas.helgegren@gmail.com>
2019-01-22 13:32:55 +0100

    Add db-migrate logic and first init migration

---
+++ migrations/sqls/20190122095424-init-up.sql
@@ -0,0 +1,9 @@
+/* Replace with your SQL commands */
+
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `email` varchar(255) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=38326 DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT;
